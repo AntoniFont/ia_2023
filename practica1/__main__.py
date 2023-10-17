a47f7dfee74a2c46573614680c66e133a1120131
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-10-17 13:12:47 +0200

    Creados agentes

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,8 +1,10 @@
-from practica1 import agent, joc
+import sys
+sys.path.append("C:\\Users\\Toniuni\\Desktop\\Curso 23-24\\IA\\ia_2023")
+from practica1 import agentProfunditat, joc
 
 
 def main():
-    quatre = joc.Taulell([agent.Agent("Miquel")])
+    quatre = joc.Taulell([agentProfunditat.Agent("Miquel")])
     quatre.comencar()
 
 
