759e0803a2d4a4736e56927fa92d2093ad4ebb8e
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-10-26 17:29:52 +0200

    Aprendidos los controles

--- practica1/agentProfunditat.py
+++ practica1/agentProfunditat.py
@@ -7,17 +7,22 @@ ClauPercepcio:
 """
 from ia_2022 import entorn
 from practica1 import joc
-from practica1.entorn import Accio
+from practica1.entorn import Accio,SENSOR,TipusCasella
 
 
 class Agent(joc.Agent):
     def __init__(self, nom):
+        self.prueba = True
         super(Agent, self).__init__(nom)
 
     def pinta(self, display):
         pass
-
-    def actua(
-            self, percepcio: entorn.Percepcio
-    ) -> entorn.Accio | tuple[entorn.Accio, object]:
-        pass
+ 
+    def actua(self, percepcio: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:
+        print(percepcio[SENSOR.MIDA])
+        print("estat tauler "  + str(percepcio[SENSOR.TAULELL]))
+        if(self.prueba == True):
+            self.prueba = False
+            return (Accio.POSAR,(2,2))
+        else: 
+            return Accio.ESPERAR
