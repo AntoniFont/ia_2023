fda664a23e7db9a9f6ae9bd02164cd91d3c66d88
JJMari <marijuanjo249@gmail.com>
2023-10-28 17:16:04 +0200

    mejoras

--- practica1/agentAstar.py
+++ practica1/agentAstar.py
@@ -1,23 +1,162 @@
 """
-
 ClauPercepcio:
     POSICIO = 0
     OLOR = 1
     PARETS = 2
 """
+import copy
+from queue import PriorityQueue
 from ia_2022 import entorn
 from practica1 import joc
-from practica1.entorn import Accio
+from practica1.entorn import Accio, SENSOR
 
+
+class Estat:
+    def __init__(self, filas_columnas, pes: int, heuristica=4, pare=None):
+        super().__init__(filas_columnas, heuristica, pare)
+        self.__pes = pes
+        self.__fn = self.heuristica + self.__pes
+
+
+    def es_meta(self):
+        return self.heuristica == 0
+
+    def calcular_heuristica_fn (self):
+        for col in range(self.lenTablero):
+            for row in range(self.lenTablero):
+                if self.tablero[col][row] == 1:
+                    h = self.mirar_combinacion(col, row)
+                    if h < self.heuristica:
+                        self.heuristica = h
+        self.fn = self.heuristica + self.__pes
+
+    def genera_fills(self) -> list:
+        estats_generats = []
+
+        for col in range(self.lenTablero):
+            for row in range(self.lenTablero):
+                if self.tablero[col][row] == 0:
+                    nou_estat = copy.deepcopy(self)
+                    nou_estat.pare = (self, (col, row))
+
+                    nou_estat.tablero[col][row] = 1
+                    nou_estat.pes += 1
+                    nou_estat.calcular_heuristica_fn()
+                    estats_generats.append(nou_estat)
+        
+        return estats_generats
+
+    # Cuando encuentra una pieza colocada, comprueba las siguientes 3 casillas horizontalmente, diagonalmente y verticalmente 
+    # para calcular la heuristica correspondiente
+    def calcula_heuristica(self, columna, fila) -> int:
+        heuristica_menor = 3
+        heuristica_menor = min(heuristica_menor, self.mirar_combinacionColumnas(columna, fila), self.mirar_combinacionFilas(columna, fila),
+                                self.mirar_combinacionDiagonalUp(columna, fila), self.mirar_combinacionDiagonalDown(columna, fila))
+        return heuristica_menor
+    
+    def mirar_combinacionColumnas(self, columna: int, fila: int) -> int:
+        heuristica = 3
+        if columna + 3 < self.lenTablero:
+            for i in range(columna + 1, columna + 4):
+                if self.tablero[i][fila] == 1:
+                    heuristica -= 1
+        return heuristica
+                
+    def mirar_combinacionFilas(self, columna: int, fila: int) -> int:
+        heuristica = 3
+        if fila + 3 < self.lenTablero:
+            for j in range(fila + 1, fila + 4):
+                if self.tablero[columna][j] == 1:
+                    heuristica -= 1
+        return heuristica
+                
+    def mirar_combinacionDiagonalDown(self, columna: int, fila: int) -> int:
+        heuristica = 3
+        if fila + 3 < self.lenTablero:
+            if columna + 3 < self.lenTablero:
+                for i in range(1, 4):
+                    if self.tablero[i + columna][i + fila] == 1:
+                        heuristica -= 1
+        return heuristica
+    
+    def mirar_combinacionDiagonalUp(self, columna: int, fila: int) -> int:
+        heuristica = 3
+        if fila - 3 >= 0:
+            if columna +3 < self.lenTablero:
+                for i in range(1, 4):
+                    if self.tablero[columna + i][fila - i] == 1:
+                        heuristica -= 1
+        return heuristica
+    
+    @property
+    def pes(self):
+        return self.__pes
+    
+    @pes.setter
+    def pes(self, pes):
+        self.__pes = pes
+
+    @property
+    def fn(self):
+        return self.__fn
+    
+    @fn.setter
+    def fn(self, fn):
+        self.__fn = fn
+    
+    def __lt__(self, other):
+        return False
 
 class Agent(joc.Agent):
     def __init__(self, nom):
         super(Agent, self).__init__(nom)
+        self.__oberts = None
+        self.__tancats = None
+        self.__accions = None
 
-    def pinta(self, display):
-        pass
+    def cerca(self, estat_inicial: Estat):
+        self.__oberts = PriorityQueue()
+        self.__tancats = set()
+        estat_actual = None
 
+        self.__oberts.put((estat_inicial.fn, estat_inicial))
+        estat_actual = None
+
+        while not self.__oberts.empty():
+            _, estat_actual = self.__oberts.get()
+            for estat_tancat in self.__tancats:
+                if estat_actual == estat_tancat:
+                    continue
+            if estat_actual.es_meta():
+                break
+
+            estats_fills = estat_actual.genera_fills()
+            for estat in estats_fills:
+                if estat not in self.__tancats:
+                    self.__oberts.put((estat.fn, estat))
+
+            self.__tancats.add(estat_actual)
+        if estat_actual.es_meta():
+            estat_actual.calcular_heuristica_fn()
+            accions = list()
+            iterador = estat_actual
+            while iterador.pare is not None:
+                pare, accio = iterador.pare
+
+                accions.append(accio)
+                iterador = pare
+            self.__accions = accions
+    
     def actua(
             self, percepcio: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
-        pass
+        estat = Estat(percepcio[SENSOR.MIDA], 0)
+        
+        if self.__accions is None:
+            self.cerca(estat)
+
+        if self.__accions:
+            accio = self.__accions.pop()
+            return Accio.POSAR, accio
+        else:
+            return Accio.ESPERAR
