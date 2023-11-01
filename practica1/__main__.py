cbccd432703cb90a8b8ae33cea42e9172110de3d
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-01 21:05:30 +0100

    Ya hay heuristica

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,12 +1,12 @@
 import sys
 sys.path.append("C:\\Users\\Toniuni\\Desktop\\Curso 23-24\\IA\\ia_2023")
 sys.path.append("C:\\Users\\juanjo\\Documents\\GitHub\\ia_2023")
-from practica1 import agentProfunditat, agentMiniMax, joc
+from practica1 import agentProfunditat, agentMaxMiniAgarrameElPepini, joc
 
 
 def main():    
 
-    quatre = joc.Taulell([agentMiniMax.Agent("Miquel"), agentMiniMax.Agent("Tomas")])
+    quatre = joc.Taulell([agentMaxMiniAgarrameElPepini.Agent("Miquel")])
     quatre.comencar()
 
 
