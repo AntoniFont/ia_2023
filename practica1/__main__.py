1efb0b597ec1e8ee96e997fcbb01a8a7ea225e89
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-02 01:53:00 +0100

    Funciona? Guess will never know

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append("C:\\Users\\Toniuni\\Desktop\\Curso 23-24\\IA\\ia_2023")
 sys.path.append("C:\\Users\\juanjo\\Documents\\GitHub\\ia_2023")
-from practica1 import agentProfunditat, agentMaxMiniAgarrameElPepini, joc
+from practica1 import agentProfunditat, agentMaxMiniAgarrameElPepini, joc,entorn
 
 
 def main():    
 
-    quatre = joc.Taulell([agentMaxMiniAgarrameElPepini.Agent("Miquel")])
+    quatre = joc.Taulell([agentMaxMiniAgarrameElPepini.Agent("Miquel",entorn.TipusCasella.CARA),
+                          agentMaxMiniAgarrameElPepini.Agent("Toni",entorn.TipusCasella.CREU)])
     quatre.comencar()
 
 
--- practica1/agentMaxMiniAgarrameElPepini.py
+++ practica1/agentMaxMiniAgarrameElPepini.py
@@ -50,14 +50,14 @@ class EstatMaxMini():
                 if(self.tauler[fila][columna] == jugador):
                     for i in range(len(ratlles_valides)):
                         ratlla = ratlles_valides[i]
-                        percentatgeRatllaCompletat = self.percentatgePatroCompletat(fila,columna,ratlla,jugador)
+                        percentatgeRatllaCompletat = self._percentatgePatroCompletat(fila,columna,ratlla,jugador)
                         if(percentatgeRatllaCompletat > puntuacions[i]):
                             puntuacions[i] = percentatgeRatllaCompletat
         return max(puntuacions)
 
     #Retorna el percentatge del patró que has completat. Si mai podràs completar el patró(perque un altre jugador
     # bloquetja la posibilitat), retorna 0.
-    def percentatgePatroCompletat(self,x_inicial,y_inicial,patro,jugador):
+    def _percentatgePatroCompletat(self,x_inicial,y_inicial,patro,jugador):
         pasosDelPatroCompletats = 0
         for x,y in patro:
             if(self._estaOcupatPerJugador(x_inicial + x,y_inicial + y,jugador)):
@@ -70,9 +70,18 @@ class EstatMaxMini():
                     pasosDelPatroCompletats = 0
         return pasosDelPatroCompletats / len(patro)
 
-    #Retorna true si existeix un cuatre en ratlla del nostre jugador.
-    def es_meta(self):
-        self.calc_score(TipusCasella.CARA) == 4
+    #Retorna true si existeix un cuatre en ratlla del nostre jugador o quan el tauler està plé.
+    def es_meta(self,jugador):
+        if(self.calc_score(jugador) == 4):
+            return True
+        hiHaEspaisLliures = False
+        for fila in self.tauler:
+            if(TipusCasella.LLIURE in fila):
+                hiHaEspaisLliures = True
+        if(not hiHaEspaisLliures):
+            return True
+        else:
+            return False
 
     #Retorna True si una posicio esta ocupada, false si no esta ocupada i false també si la posicio esta
     #fora del tauler
@@ -83,22 +92,24 @@ class EstatMaxMini():
         else :
             return self.tauler[fila][columna] == jugador
         
-    def genera_fill(self):
+    def genera_fill(self,jugador):
         fills = []
         for fila in range(len(self.tauler)):
             for columna in (range(len(self.tauler[0]))):
                 if(self.tauler[fila][columna] == TipusCasella.LLIURE):
                     nouTauler = deepcopy(self.tauler)
                     nouAccionsPrevies = deepcopy(self.accions_previes)
-                    nouTauler[fila][columna] = TipusCasella.CARA
+                    nouTauler[fila][columna] = jugador
                     nouAccionsPrevies.append((Accio.POSAR,(fila,columna)))
-                    fills.append(EstatProfunditat(nouTauler,nouAccionsPrevies)) 
+                    fills.append(EstatMaxMini(nouTauler,nouAccionsPrevies)) 
         return fills
 
 
 
+
 class Agent(joc.Agent):
-    def __init__(self, nom):
+    def __init__(self,nom,jugador):
+        self.jugador = jugador
         self.primeraExecucio = True       
         self.__ACCIONSPERFER = [
             (Accio.POSAR,(3,5)),
@@ -106,7 +117,7 @@ class Agent(joc.Agent):
             (Accio.POSAR,(5,3)),
             (Accio.POSAR,(6,2))
         ]
-        
+        self.prova = 0
         self.numACCIONSFETES = 0
         super(Agent, self).__init__(nom)
 
@@ -114,7 +125,12 @@ class Agent(joc.Agent):
         pass
  
     def actua(self, percepcio: entorn.Percepcio) -> entorn.Accio | tuple[entorn.Accio, object]:        
-        print(EstatMaxMini(percepcio[SENSOR.TAULELL]).calc_score(TipusCasella.CARA))
+        if(self.primeraExecucio):
+            estat,score = self.minimax(EstatMaxMini(percepcio[SENSOR.TAULELL]),float("-inf"),float("+inf"),True)
+            print(estat)
+            print(score)
+            self.primeraExecucio = False
+        
         if(self.numACCIONSFETES > len(self.__ACCIONSPERFER) - 1):
             return Accio.ESPERAR
         else:
@@ -122,4 +138,33 @@ class Agent(joc.Agent):
             self.numACCIONSFETES = self.numACCIONSFETES + 1
         return accio
         
-    
+    def minimax(self,estat,alpha,beta,torn_de_max):
+        if(self.prova % 1000 == 0):
+            print("minmax",self.prova)
+        self.prova = self.prova + 1
+        if(estat.es_meta(self.jugador)):
+            return estat.calc_score(self.jugador)
+        else:
+            fills = estat.genera_fill(self.jugador)
+            if(torn_de_max):
+                maxim = float("-inf")
+                for estat_fill in fills:
+                    recursiu = self.minimax(estat_fill,alpha,beta,not torn_de_max)
+                    maxim = max(maxim,recursiu)
+                    alpha = max(alpha,recursiu)
+                    if(beta <= alpha):
+                        break
+                return maxim
+            else:
+                minim = float("+inf")
+                for estat_fill in fills:
+                    recursiu = self.minimax(estat_fill,alpha,beta,not torn_de_max)
+                    minim = min(minim,recursiu)
+                    beta = min(beta,recursiu)
+                    if(beta <= alpha):
+                        break
+                return minim
+    def evaluar(self,estat):
+        if estat.es_meta(self.jugador):
+            return estat.calc_score(self.jugador)
+
