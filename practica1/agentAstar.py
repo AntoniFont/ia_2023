0611e75a355d028fb3b8bbc300bc1181c050c42d
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-02 15:52:18 +0100

    Squash
    Todos usan la misma función meta
    
    Fora aquest print

--- practica1/agentAstar.py
+++ practica1/agentAstar.py
@@ -17,48 +17,83 @@ class Estat():
         self.tauler = tauler
         self.accions_previes = accionsPrevies
         self.heuristica = self.calcul_heuristica() if accionsPrevies else 1000
-        
-    #Retorna true si existeix un cuatre en ratlla del nostre jugador.
-    def es_meta(self):
+
+        #Cerca per tot el tauler i retorna la ratlla amb posibilitats de ser completada
+    #mes gran que hi hagi del jugador seleccionat 
+    #Una ratlla de 3, ex:(x x x o) que mai pugi ser completada contará com a 0.
+    def calc_score(self,jugador):
+        #
+        #(x) x x x
+        #
+        #
+        HORITZONTAL = ((0,0),(1,0),(2,0),(3,0))
+        # (x)
+        #  x
+        #  x
+        #  x
+        VERTICAL = ((0,0),(0,1),(0,2),(0,3))
+        # (x)
+        #   x
+        #    x
+        #     x
+        DIAGONAL_INF = ((0,0),(1,1),(2,2),(3,3))
+        #        x
+        #      x
+        #    x
+        # (x)
+        DIAGONAL_SUP = ((0,0),(1,-1),(2,-2),(3,-3))
+        ratlles_valides = (HORITZONTAL,VERTICAL,DIAGONAL_INF,DIAGONAL_SUP)    
+        puntuacions = [0, # Ratlla horitzontal mes llarga, en percentatge respecte a 4 en ratlla
+                       0, # Ratlla vertical mes llarga, en percentatge respecte a 4 en ratlla
+                       0, # Ratlla diagonal_inf mes llarga, en percentatge respecte a 4 en ratlla
+                       0] # Ratlla diagonal_sup mes llarga, en percentatge respecte a 4 en ratlla
         for fila in range(len(self.tauler)):
             for columna in (range(len(self.tauler[0]))):
-                # Suposam que el lloc sempre serà cuatre en ratlla i no un altre número en ratlla.
-                if(self.tauler[fila][columna] == TipusCasella.CARA):
-                    # (x) x x x
-                    #
-                    #
-                    #
-                    if(self._estaOcupatPerCara(fila +1,columna) and self._estaOcupatPerCara(fila +2, columna) and self._estaOcupatPerCara(fila + 3,columna)):
-                        return True
-                    # (x)
-                    #  x
-                    #  x
-                    #  x
-                    if(self._estaOcupatPerCara(fila,columna +1) and self._estaOcupatPerCara(fila, columna +2) and self._estaOcupatPerCara(fila,columna +3)):
-                        return True
-                    # (x)
-                    #   x
-                    #    x
-                    #     x
-                    if(self._estaOcupatPerCara(fila +1,columna +1) and self._estaOcupatPerCara(fila+2, columna +2) and self._estaOcupatPerCara(fila+3,columna +3)):
-                        return True
-                    #        x
-                    #      x
-                    #    x
-                    # (x)    
-                    if(self._estaOcupatPerCara(fila +1,columna -1) and self._estaOcupatPerCara(fila+2, columna -2) and self._estaOcupatPerCara(fila+3,columna -3)):
-                        return True
-        return False         
+                if(self.tauler[fila][columna] == jugador):
+                    for i in range(len(ratlles_valides)):
+                        ratlla = ratlles_valides[i]
+                        percentatgeRatllaCompletat = self._percentatgePatroCompletat(fila,columna,ratlla,jugador)
+                        if(percentatgeRatllaCompletat > puntuacions[i]):
+                            puntuacions[i] = percentatgeRatllaCompletat
+        return max(puntuacions)
+
+    #Retorna el percentatge del patró que has completat. Si mai podràs completar el patró(perque un altre jugador
+    # bloquetja la posibilitat), retorna 0.
+    def _percentatgePatroCompletat(self,x_inicial,y_inicial,patro,jugador):
+        pasosDelPatroCompletats = 0
+        for x,y in patro:
+            if(self._estaOcupatPerJugador(x_inicial + x,y_inicial + y,jugador)):
+                pasosDelPatroCompletats = pasosDelPatroCompletats + 1
+            elif(self._estaOcupatPerJugador(x_inicial + x,y_inicial + y,TipusCasella.LLIURE)):
+                pass
+                #No fer res, encara hi ha potencial per completar mes patró
+            else: # Esta ocupat per jugador rival 
+                if(pasosDelPatroCompletats != len(patro)): #Mai podràs completar tot el patró, puntuació 0.
+                    pasosDelPatroCompletats = 0
+        return pasosDelPatroCompletats / len(patro)
+
+    #Retorna true si existeix un cuatre en ratlla del nostre jugador o quan el tauler està plé.
+    def es_meta(self,jugador):
+        if(self.calc_score(jugador) == 4):
+            return True
+        hiHaEspaisLliures = False
+        for fila in self.tauler:
+            if(TipusCasella.LLIURE in fila):
+                hiHaEspaisLliures = True
+        if(not hiHaEspaisLliures):
+            return True
+        else:
+            return False
 
     #Retorna True si una posicio esta ocupada, false si no esta ocupada i false també si la posicio esta
     #fora del tauler
-    def _estaOcupatPerCara(self,fila,columna):
+    def _estaOcupatPerJugador(self,fila,columna,jugador):
         #Suposam que el tauler es sempre un quadrat
         if fila < 0 or columna < 0 or fila > len(self.tauler)-1 or columna > len(self.tauler)-1:
             return False
         else :
-            return self.tauler[fila][columna] == TipusCasella.CARA
-        
+            return self.tauler[fila][columna] == jugador
+
     def calcul_heuristica(self) -> int:
 
         n = len(self.tauler[0])
@@ -171,7 +206,7 @@ class Agent(joc.Agent):
         while not self.__oberts.empty():
             estatActual = self.__oberts.get(0)
             self.__tancats.append(estatActual)
-            if(estatActual.es_meta()):
+            if(estatActual.es_meta(self.jugador)):
                 self.__accions = estatActual.accions_previes
                 return True
             else:
