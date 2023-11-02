2e344695d6f96fc7a943abe72d439ca8e9a9431e
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-02 16:07:29 +0100

    Revert "Squash"
    This reverts commit 0611e75a355d028fb3b8bbc300bc1181c050c42d.

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -2,14 +2,12 @@ import sys
 sys.path.append("C:\\Users\\Toniuni\\Desktop\\Curso 23-24\\IA\\ia_2023")
 sys.path.append("C:\\Users\\juanjo\\Documents\\GitHub\\ia_2023")
 sys.path.append("C:\\Users\\marij\\Desktop\\IA")
-from practica1 import agentAstar, agentMaxMiniAgarrameElPepini, agentProfunditat,joc,entorn
+from practica1 import agentAstar, agentMaxMiniAgarrameElPepini, joc,entorn
 
 
 def main():    
 
-    #quatre = joc.Taulell([agentAstar.Agent("Miquel")])
-    quatre = joc.Taulell([agentProfunditat.Agent("Miquel")])
-    
+    quatre = joc.Taulell([agentAstar.Agent("Miquel")])
     quatre.comencar()
 
 
