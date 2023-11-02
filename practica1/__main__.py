042031d36cc4737a43efe9da5c7abb49f6a9044d
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-02 14:17:23 +0100

    Ya furula minimax

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,13 +1,12 @@
 import sys
 sys.path.append("C:\\Users\\Toniuni\\Desktop\\Curso 23-24\\IA\\ia_2023")
 sys.path.append("C:\\Users\\juanjo\\Documents\\GitHub\\ia_2023")
-from practica1 import agentProfunditat, agentMaxMiniAgarrameElPepini, joc,entorn
+from practica1 import agentProfunditat, agentMaxMiniAgarrameElPepini,agentAstar, joc,entorn
 
 
 def main():    
 
-    quatre = joc.Taulell([agentMaxMiniAgarrameElPepini.Agent("Miquel",entorn.TipusCasella.CARA),
-                          agentMaxMiniAgarrameElPepini.Agent("Toni",entorn.TipusCasella.CREU)])
+    quatre = joc.Taulell([agentAstar.Agent("Miquel")])
     quatre.comencar()
 
 
